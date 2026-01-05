50f5573a7ddddf66982dd180d2b72706f8ac7820
Christopher Lagunas <148503662+CodingwithCoque@users.noreply.github.com>
2026-01-04 23:32:55 -0700

    Delete Anaheim Ballet/src directory

--- Anaheim Ballet/src/pages/Events.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Events() {
-  return <h1>Events Page Coming Soon</h1>;
-}
-
-export default Events;
--- Anaheim Ballet/src/pages/Gallery.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Gallery() {
-  return <h1>Gallery Page Coming Soon</h1>;
-}
-
-export default Gallery;
